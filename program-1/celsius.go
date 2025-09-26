8165b36017c3e1363e162211f20ea56049267ef7
default <marco.cantusci@stu.ocsb.ca>
2025-09-26 04:47:03 +0000

    Completed program 1

---
+++ program-1/celsius.go
@@ -0,0 +1,32 @@
+/**
+ * @author Marco Cantusci
+ * @verion 1.0.0
+ * @date 2025-09-26
+ * @fileoverview Celsius to Fahrenheit
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+
+	// state question
+	fmt.Println("What is 34 Celsius in Fahrenheit?")
+
+	// formula
+	fmt.Println("F = 9 / 5 * C + 32")
+
+	// put information into the variables
+	fmt.Println("F = 9 / 5 * 34 + 32")
+
+	// solve
+	fmt.Println("F = " + fmt.Sprint(9.0/5.0) + " * 34 + 32")
+
+	fmt.Println("F = " + fmt.Sprint(1.8*34) + " + 32")
+
+	fmt.Println("F = " + fmt.Sprint(61.2+32))
+
+	fmt.Println("\nDone.")
+
+}
---
+++ program-1/celsius.ts
@@ -0,0 +1,26 @@
+/**
+ * @author Marco Cantusci
+ * @verion 1.0.0
+ * @date 2025-09-26
+ * @fileoverview Celsius to Fahrenheit
+ */
+
+
+// state question
+console.log("What is 34 Celsius in Fahrenheit?")
+
+// formula
+console.log("F = 9 / 5 * C + 32")
+
+
+// put numbers into known variables
+console.log("F = 9 / 5 * 34 + 32")
+
+// solve
+console.log("F = " + (9.0 / 5.0) + " * 34 + 32")
+
+console.log("F = " + (1.8*34) + " + 32")
+
+console.log("F = " + (61.2 + 32))
+
+console.log("\nDone.")
