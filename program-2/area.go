b1091167f3bb23f7532ee24052d4069c2d76f910
default <marco.cantusci@stu.ocsb.ca>
2025-09-26 05:14:32 +0000

    Completed part 2

---
+++ program-2/area.go
@@ -0,0 +1,33 @@
+/**
+ * @author Marco Cantusci
+ * @verion 1.0.0
+ * @date 2025-09-26
+ * @fileoverview Area of circle
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+
+	// state question
+	fmt.Println("What is the area of a circle with a radius of 14 cm?")
+
+	// formula
+	fmt.Println("A = pi * r * r")
+
+	// put in known variables
+	fmt.Println("A = 3.14 * 14 * 14")
+
+	// solve
+	fmt.Println("A = " + fmt.Sprint(3.14*14) + " * 14")
+
+	fmt.Println("A = " + fmt.Sprint(43.96*14))
+
+	// final statement
+	fmt.Println("The area of the circle is ~615.44cm")
+	
+	fmt.Println("\nDone.")
+
+}
---
+++ program-2/area.ts
@@ -0,0 +1,25 @@
+/**
+ * @author Marco Cantusci
+ * @verion 1.0.0
+ * @date 2025-09-26
+ * @fileoverview Area of circle
+ */
+
+// state question
+console.log("What is the area of a circle with a radius of 14 cm?")
+
+// state formula
+console.log("A = pi * r * r")
+
+// enter in known information
+console.log("A = 3.14 * 14 * 14")
+
+// solve
+console.log("A = " + (3.14*14) + " * 14")
+
+console.log("A = " + (43.96*14))
+
+// final statement
+console.log("The area of the circle is ~615.44cm")
+
+console.log("\nDone.")
